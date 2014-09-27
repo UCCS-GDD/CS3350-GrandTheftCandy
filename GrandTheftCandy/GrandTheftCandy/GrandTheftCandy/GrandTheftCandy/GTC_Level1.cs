0bc6f756bb9b0090e75f77a58c66b5cf13271efa
mrawlin2 <mrawlin2@uccs.edu>
2014-09-26 21:35:28 -0600

    Project thus far
    Without the newest artwork, and with some of the new collision
    detection. About where it was on Thursday.

---
+++ GrandTheftCandy/GrandTheftCandy/GrandTheftCandy/GrandTheftCandy/GTC_Level1.cs
@@ -0,0 +1,225 @@
+/*
+ * Grand Theft Candy
+ * Level 1 - Project 1
+ * Simple XNA Game
+ * Matthew Rawlins
+ * Jeffrey St.Germain  
+ * Jason Noujok
+ * Danyelle Barnett
+ * Lauren Benson
+*/
+
+/*
+ *---------------------Notes--------------------
+ * All objects using the Sprite_Base_Class use the first constructor.
+ * The second is for the "mall" floors and background objects.
+ * Draw Order for background objects should be within the range of 0-4.
+ * Normal objects are automatically calculated based on the starting position
+ * and changes as the sprite moves up and down. (Range: 5-85)
+ * Please use 5 pixel incriments when placing sprites, with a minimum Y value of 200.
+ * Note that the starting position for a sprite is the center of the sprite.
+ * The top 200 is reserved for the background wall.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace GrandTheftCandy
+{
+   /// <summary>
+   /// This is the main type for your game.
+   /// </summary>
+   public class GTC_Level1 : Microsoft.Xna.Framework.Game
+   {
+
+      #region Member Variables
+      GraphicsDeviceManager graphics;
+      public SpriteBatch spriteBatch;
+
+      Player_Controlled_Sprite player;
+      Sprite_Base_Class blue;
+      Sprite_Base_Class red;
+      Sprite_Base_Class redCollisionTest;
+      Sprite_Base_Class winScreen;
+      Sprite_Base_Class gameOver;
+
+      Vector2 screenCenter;
+      Vector2 blueStart;
+      Vector2 redStart;
+
+      Texture2D lineTexture;
+      #endregion
+
+      #region Constructors
+
+      public GTC_Level1()
+      {
+         graphics = new GraphicsDeviceManager(this);
+         graphics.PreferredBackBufferWidth = 800;
+         graphics.PreferredBackBufferHeight = 600;
+         this.IsMouseVisible = true;
+         Content.RootDirectory = "Content";
+      }
+
+      #endregion
+
+      #region Overridden Functions
+      /// <summary>
+      /// Allows the game to perform any initialization it needs to before starting to run.
+      /// This is where it can query for any required services and load any non-graphic
+      /// related content.  Calling base.Initialize will enumerate through any components
+      /// and initialize them as well.
+      /// </summary>
+      protected override void Initialize()
+      {
+         lineTexture = this.Content.Load<Texture2D>(@"Resources\Images\Line");
+
+         screenCenter = new Vector2 ( ( graphics.GraphicsDevice.Viewport.Width / 2 ), ( graphics.GraphicsDevice.Viewport.Height / 2 ) );
+         blueStart = new Vector2 ( ( graphics.GraphicsDevice.Viewport.Width / 4 ), ( graphics.GraphicsDevice.Viewport.Height / 2 ) );
+         redStart = new Vector2 ( ( graphics.GraphicsDevice.Viewport.Width * 3 / 4 ), ( graphics.GraphicsDevice.Viewport.Height / 2 ) );
+
+         player = new Player_Controlled_Sprite(this, @"Resources\Images\Circle", screenCenter, Color.White, true, "Player");
+         blue = new Sprite_Base_Class(this, @"Resources\Images\bluesquare", blueStart, Color.White, true, "Blue");
+         red = new Sprite_Base_Class(this, @"Resources\Images\redsquare", redStart, Color.White, true, "Red");
+         redCollisionTest = new Sprite_Base_Class (this, @"Resources\Images\redsquare", new Vector2 (400, 100), Color.White, true, "Red Collision");
+
+         Splash_Screen splashScreen = new Splash_Screen(this, @"Resources\Images\SplashScreen", screenCenter, Color.White, "Splash Screen");
+
+         gameOver = new Sprite_Base_Class(this, @"Resources\Images\gameover0", screenCenter, false, 500, "Game Over 1");
+         gameOver.Visible = false;
+
+         winScreen = new Sprite_Base_Class(this, @"Resources\Images\winner", screenCenter, false, 500, "Game Over 2");
+         winScreen.Visible = false;
+
+         Song backgroundSound = Content.Load<Song>(@"Resources\Sounds\gameMusic");
+         MediaPlayer.Play(backgroundSound);
+
+         base.Initialize();
+        }
+
+      /// <summary>
+      /// LoadContent will be called once per game and is the place to load
+      /// all of your content.
+      /// </summary>
+      protected override void LoadContent()
+      {
+         // Create a new SpriteBatch, which can be used to draw textures.
+         spriteBatch = new SpriteBatch(GraphicsDevice);
+      }
+
+      /// <summary>
+      /// UnloadContent will be called once per game and is the place to unload
+      /// all content.
+      /// </summary>
+      protected override void UnloadContent()
+      {
+      }
+
+      /// <summary>
+      /// Allows the game to run logic such as updating the world,
+      /// checking for collisions, gathering input, and playing audio.
+      /// </summary>
+      /// <param name="gameTime">Provides a snapshot of timing values.</param>
+      protected override void Update(GameTime gameTime)
+      {
+         // Allows the game to exit.
+         KeyboardState keyboardState = Keyboard.GetState ();
+
+         if (keyboardState.IsKeyDown (Keys.Escape) ||
+            GamePad.GetState (PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+         {
+            this.Exit ();
+         }
+
+         if (player.collidesHorizontally(red))
+         {
+            player.Visible = false;
+            red.Visible = false;
+            blue.Visible = false;
+
+            System.Threading.Thread.Sleep (50);
+            winScreen.Visible = true;
+         }
+
+         if (player.collidesWithAbove(blue) || player.collidesWithBelow(blue))
+         {
+            player.Visible = false;
+            red.Visible = false;
+            blue.Visible = false;
+
+            System.Threading.Thread.Sleep (50);
+            gameOver.Visible = true;
+         }
+
+         base.Update(gameTime);
+      }
+
+      /// <summary>
+      /// This is called when the game should draw itself.
+      /// </summary>
+      /// <param name="gameTime">Provides a snapshot of timing values.</param>
+      protected override void Draw(GameTime gameTime)
+      {
+         GraphicsDevice.Clear(Color.CornflowerBlue);
+
+         // Draw a vertical line through the center.
+         Vector2 verticalStart = new Vector2(400,0);
+         Vector2 verticalEnd = new Vector2(400, 600);
+         DrawLine(lineTexture, this.spriteBatch, verticalStart, verticalEnd, true );
+
+         // Draw a horizontal line through the center.
+         Vector2 horizontalStart = new Vector2 ( 0, 300 );
+         Vector2 horizontalEnd = new Vector2 ( 800, 300 );
+         DrawLine ( lineTexture, this.spriteBatch, horizontalStart, horizontalEnd, false );
+
+         base.Draw(gameTime);
+      }
+
+      #endregion
+
+      #region Functions
+
+      /// <summary>
+      /// Draws either a vertical line or horizontal line using a starting point and an ending point.
+      /// </summary>
+      /// <param name="a_lineTexture"></param>
+      /// <param name="spriteBatch"></param>
+      /// <param name="a_startingPosition"></param>
+      /// <param name="a_endingPosition"></param>
+      /// <param name="a_drawVertical"></param>
+      public static void DrawLine(Texture2D a_lineTexture, SpriteBatch spriteBatch, 
+         Vector2 a_startingPosition, Vector2 a_endingPosition, bool a_drawVertical)
+      {
+         if ( a_drawVertical )
+         {
+            spriteBatch.Begin();
+            while ( a_startingPosition.Y < a_endingPosition.Y )
+            {
+               spriteBatch.Draw(a_lineTexture, a_startingPosition, Color.Black);
+               a_startingPosition.Y++;
+            }
+            spriteBatch.End();
+         }
+         else
+         {
+            spriteBatch.Begin ();
+            while ( a_startingPosition.X < a_endingPosition.X )
+            {
+               spriteBatch.Draw ( a_lineTexture, a_startingPosition, Color.Black );
+               a_startingPosition.X++;
+            }
+            spriteBatch.End ();
+         }
+      }
+
+      #endregion
+   }
+}
---
+++ GrandTheftCandy/GrandTheftCandy/GrandTheftCandy/GrandTheftCandy/Sprite_Base_Class.cs
@@ -0,0 +1,380 @@
+﻿/*
+ * TODO:
+*/
+
+/*
+ * Notes: DrawOrder is calculated by where a sprite's center is.
+ * An offset of 5 is the minimum for movable sprites.
+ * This allows for 5 levels for background objects.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace GrandTheftCandy
+{
+   class Sprite_Base_Class : DrawableGameComponent
+   {
+      #region Member Variables
+
+      protected string m_textureFileName;
+      protected Texture2D m_textureImage;
+      protected Color m_spriteRenderColor;
+      protected Vector2 m_spriteCenter;
+      protected Vector2 m_spritePosition;
+      protected bool m_isSpriteCollidable;
+      protected String m_spriteName;
+
+      #endregion
+
+      #region Constructors
+
+      /// <summary>
+      /// Basic constructor for sprites. Any objects that need to be drawn should use this constructor.
+      /// </summary>
+      /// <param name="a_game"></param>
+      /// <param name="a_textureFileName"></param>
+      /// <param name="a_startingPosition"></param>
+      /// <param name="a_renderColor"></param>
+      /// <param name="a_collidable"></param>
+      public Sprite_Base_Class(Game a_game, String a_textureFileName, Vector2 a_startingPosition, 
+         Color a_renderColor, bool a_collidable, String a_SpriteName)
+         : base(a_game)
+      {
+         m_textureFileName = a_textureFileName;
+         m_spritePosition = a_startingPosition;
+         this.DrawOrder = (((int)m_spritePosition.Y - 200) / 5) + 5;
+         m_spriteRenderColor = a_renderColor;
+         m_isSpriteCollidable = a_collidable;
+         m_spriteName = a_SpriteName;
+         a_game.Components.Add(this);
+      }
+
+      /// <summary>
+      /// Special constructor for when the draw order needs to be specified.
+      /// An example of this is the floor or background walls.
+      /// </summary>
+      /// <param name="a_game"></param>
+      /// <param name="a_textureFileName"></param>
+      /// <param name="a_startingPosition"></param>
+      /// <param name="a_collidable"></param>
+      /// <param name="a_drawOrder"></param>
+      public Sprite_Base_Class (Game a_game, String a_textureFileName, Vector2 a_startingPosition, 
+         bool a_collidable, int a_drawOrder, String a_SpriteName)
+         : base (a_game)
+      {
+         m_textureFileName = a_textureFileName;
+         m_spritePosition = a_startingPosition;
+         this.DrawOrder = a_drawOrder;
+         m_spriteRenderColor = Color.White;
+         m_isSpriteCollidable = a_collidable;
+         m_spriteName = a_SpriteName;
+         a_game.Components.Add (this);
+      }
+
+      #endregion
+
+      #region Getters and Setters
+
+      public Vector2 spriteCenter
+      {
+         get
+         {
+               return m_spriteCenter;
+         }
+         set
+         {
+            m_spriteCenter = spriteCenter;
+         }
+      }
+
+      public Vector2 spritePosition
+      {
+         get
+         {
+            return m_spritePosition;
+         }
+         set
+         {
+            m_spritePosition = spritePosition;
+         }
+      }
+
+      public String textureFileName
+      {
+         get
+         {
+            return m_textureFileName;
+         }
+         set
+         {
+            m_textureFileName = textureFileName;
+         }
+        }
+
+      public String spriteName
+      {
+         get
+         {
+            return m_textureFileName;
+         }
+         set
+         {
+            m_spriteName = spriteName;
+         }
+      }
+
+      public bool isSpriteCollidable
+      {
+         get
+         {
+            return m_isSpriteCollidable;
+         }
+         set
+         {
+            m_isSpriteCollidable = isSpriteCollidable;
+         }
+      }
+
+      /// <summary>
+      /// Returns the bounding box for the sprite.
+      /// </summary>
+      public Rectangle boundingBox
+      {
+         get
+         {
+            return new Rectangle ((int)(m_spritePosition.X - m_spriteCenter.X),
+               (int)(m_spritePosition.Y - m_spriteCenter.Y),
+               m_textureImage.Width, m_textureImage.Height);
+         }
+      }
+
+      #endregion
+
+      #region Overridden Functions
+
+      protected override void LoadContent()
+      {
+         m_textureImage = Game.Content.Load<Texture2D>(m_textureFileName);
+         m_spriteCenter = new Vector2 (m_textureImage.Width * 0.5f, m_textureImage.Height * 0.5f);
+
+         base.LoadContent();
+      }
+
+      public override void Initialize()
+      {
+         base.Initialize();
+      }
+
+      public override void Update(GameTime gameTime)
+      {
+         base.Update(gameTime);
+      }
+
+      public override void Draw(GameTime gameTime)
+      {
+         SpriteBatch sb = ((GTC_Level1)this.Game).spriteBatch;
+
+         sb.Begin();
+         sb.Draw(m_textureImage, m_spritePosition, null, m_spriteRenderColor, 0f, m_spriteCenter,
+            1.0f, SpriteEffects.None, 0f);
+         sb.End();
+
+         base.Draw(gameTime);
+      }
+
+      #endregion
+
+      #region Functions
+
+      public bool isWithinSpriteBoundry (Sprite_Base_Class a_sprite)
+      {
+         if (this.boundingBox.Intersects (a_sprite.boundingBox))
+         {
+            return true;
+         }
+         return false;
+      }
+
+      /// <summary>
+      /// Determines if two sprites collide when on the same srawing level.
+      /// </summary>
+      /// <param name="a_sprite"></param>
+      /// <returns></returns>
+      public bool collidesHorizontally (Sprite_Base_Class a_sprite)
+      {
+         if (this.DrawOrder == a_sprite.DrawOrder && a_sprite.isSpriteCollidable)
+         {
+            return isWithinSpriteBoundry (a_sprite);
+         }
+         return false;
+      }
+
+      public bool collidesWithBelow (Sprite_Base_Class a_sprite)
+      {
+         if ((a_sprite.DrawOrder < this.DrawOrder) && a_sprite.isSpriteCollidable)
+         {
+            return isWithinSpriteBoundry (a_sprite);
+         }
+         return false;
+      }
+
+      public bool collidesWithAbove (Sprite_Base_Class a_sprite)
+      {
+         if ((a_sprite.DrawOrder > this.DrawOrder) && a_sprite.isSpriteCollidable)
+         {
+            return isWithinSpriteBoundry (a_sprite);
+         }
+         return false;
+      }
+
+      #endregion
+
+   } // End Sprite_Base_Class.
+
+   class Player_Controlled_Sprite : Sprite_Base_Class
+   {
+      #region Member Variables
+
+      #endregion
+
+      #region Constructors
+
+      public Player_Controlled_Sprite (Game a_game, String a_textureFileName, Vector2 a_startingPosition, 
+         Color a_renderColor, bool a_collidable, String a_SpriteName)
+         : base(a_game, a_textureFileName, a_startingPosition, a_renderColor, a_collidable, a_SpriteName)
+      {
+
+      }
+
+      #endregion
+
+      #region Getters and Setters
+
+      #endregion
+
+      #region Overridden Functions
+
+      protected override void LoadContent()
+      {
+         base.LoadContent();
+      }
+
+      public override void Initialize()
+      {
+         base.Initialize();
+      }
+
+      public override void Update(GameTime gameTime)
+      {
+         KeyboardState keyboardInput = Keyboard.GetState();
+
+         if (keyboardInput.IsKeyDown(Keys.S) || keyboardInput.IsKeyDown(Keys.Down)) //backward
+         {
+            m_spritePosition.Y += 5;
+            this.DrawOrder++;
+            Sprite_Base_Class[] spriteList = new Sprite_Base_Class[this.Game.Components.Count];
+            this.Game.Components.CopyTo (spriteList, 0);
+            for (int i = 0; i < spriteList.Length; i++)
+            {
+               if (this.collidesHorizontally (spriteList[i]) && this.spriteName != spriteList[i].spriteName)
+               {
+                  m_spritePosition.Y -= 5;
+                  this.DrawOrder--;
+               }
+            }
+
+               m_spritePosition.Y += 5;
+            this.DrawOrder++;
+         }
+         if (keyboardInput.IsKeyDown(Keys.A) || keyboardInput.IsKeyDown(Keys.Left)) //left
+         {
+            m_spritePosition.X -= 5;
+         }
+         if (keyboardInput.IsKeyDown(Keys.D) || keyboardInput.IsKeyDown(Keys.Right)) //right
+         {
+            m_spritePosition.X += 5;
+         }
+         if (keyboardInput.IsKeyDown(Keys.W) || keyboardInput.IsKeyDown(Keys.Up)) //forward
+         {
+            m_spritePosition.Y -= 5;
+            this.DrawOrder--;
+         }
+         base.Update(gameTime);
+      }
+
+      public override void Draw(GameTime gameTime)
+      {
+         base.Draw(gameTime);
+      }
+
+      #endregion
+
+      #region Functions
+
+      #endregion
+
+   } // End Player_Controlled_Sprite Class.
+
+   class Splash_Screen : Sprite_Base_Class
+   {
+      #region Member Variables
+
+      #endregion
+
+      #region Constructors
+
+      public Splash_Screen (Game a_game, String a_textureFileName, Vector2 a_startingPosition, Color a_renderColor, String a_SpriteName)
+         : base(a_game, a_textureFileName, a_startingPosition, a_renderColor, false, a_SpriteName)
+      {
+         this.DrawOrder = 500; 
+      }
+
+      #endregion
+
+      #region Getters and Setters
+
+      #endregion
+
+      #region Overridden Functions
+
+      protected override void LoadContent()
+      {
+         base.LoadContent();
+      }
+
+      public override void Initialize()
+      {
+         base.Initialize();
+      }
+
+      public override void Update(GameTime gameTime)
+      {
+         MouseState mouse = Mouse.GetState();
+
+         if (mouse.LeftButton == ButtonState.Pressed)
+         {
+            this.Visible = false;
+            this.DrawOrder = 100;
+         }
+
+         base.Update(gameTime);
+      }
+
+      public override void Draw(GameTime gameTime)
+      {
+         base.Draw(gameTime);
+      }
+
+      #endregion
+
+      #region Functions
+
+      #endregion
+   } // End Splash_Screen Class.
+}
